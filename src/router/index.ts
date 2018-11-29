c728629c75ed124fa6059972a2f1c61cba624028
DevinDon <DevinDon@Foxmail.com>
2018-11-30 00:20:08 +0800

    Optimization: Export all paths as one variable.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,2 +1,16 @@
-export * from './post';
-export * from './get';
+import { AllPaths } from 'koa-backend-server';
+import deletePaths from './delete';
+import getPaths from './get';
+import patchPaths from './patch';
+import postPaths from './post';
+import putPaths from './put';
+
+export const allPaths: AllPaths = {
+  DELETE: deletePaths,
+  GET: getPaths,
+  PATCH: patchPaths,
+  POST: postPaths,
+  PUT: putPaths
+};
+
+export default allPaths;

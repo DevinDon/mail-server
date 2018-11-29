abaa586d7050c4904c8397b8c8641552c0767cf5
DevinDon <DevinDon@Foxmail.com>
2018-11-30 00:17:33 +0800

    Update: Use ormconfig.json to connect database, set API version.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,10 @@
 import { Server } from 'koa-backend-server';
-import { ConnectionOptions } from 'typeorm';
-import { Mail, User } from './entity';
-import { postPaths, getPaths } from './router';
-
-const database: ConnectionOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  database: 'docker',
-  username: 'docker',
-  password: 'docker',
-  entities: [
-    Mail,
-    User
-  ],
-  synchronize: false,
-  logging: true
-};
+import allPaths from './router';
 
 const server: Server = new Server({
-  database,
+  database: true,
   keys: ['mail', 'server'],
-  paths: { GET: getPaths, POST: postPaths },
-  type: 'HTTP'
+  paths: allPaths,
+  type: 'HTTP',
+  version: 'v1'
 });

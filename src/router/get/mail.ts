92f7e796ae96a85c0599ce4db1c49d8f0fe7c6fa
DevinDon <DevinDon@Foxmail.com>
2018-12-01 20:16:01 +0800

    Update: Update mail.

--- src/router/get/mail.ts
+++ src/router/get/mail.ts
@@ -2,13 +2,11 @@ import { AMiddleware } from 'koa-backend-server';
 import { Mail } from '../../entity';
 
 export const mail: AMiddleware = async (c, next) => {
-  const data = c.params;
-  const result = await Mail.findOne({ id: data.id }, { relations: ['content'] });
+  const request = c.query;
+  const data = await Mail.findOne({ id: request.id }, { relations: ['content'] });
   c.body = {
-    status: result ? true : false,
-    router: c.params,
-    query: c.query,
-    data: result
+    status: data ? true : false,
+    data
   };
   next();
 };
